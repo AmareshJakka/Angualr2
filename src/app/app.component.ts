5bc607c7e00c624c816e566ecd565dea8cf1698e
AmareshJakka <dilip@maainfotek.com>
2018-10-27 16:16:46 +0530

    Angular file for learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+image :string;
+constructor(){
+  this.image ='./src/assets/groups.png';
+}
+  
+
 }
